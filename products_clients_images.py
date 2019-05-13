6be08e63ee5dd229f71a341dc79b9f59be07a0bc
Rishat Sharafiev <rishatsharafiev@ya.ru>
2019-05-13 03:22:07 +0300

    Add logger

--- products_clients_images.py
+++ products_clients_images.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
-from ...utils.db.connection import get_connection
+from utils.logging.loggers import create_rotating_log
+from utils.logging.decorators import exception
+
+log_file = "exception.log"
+logger = create_rotating_log(log_file)
+
+@exception(logger)
+def main():
+    raise Exception('Hello')
 
 if __name__ == '__main__':
-    a = get_connection()
-    print(a)
+    main()
